260eeb560914f6f0b20b64994237a15bd9557b3b
Diego Cardoso <diegocar448@hotmail.com>
2021-05-02 16:55:46 -0300

    model transaction

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import './models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
@@ -5,20 +6,50 @@ main() => runApp(ExpensesApp());
 class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Despesas Pessoais"),
-      ),
-      body: Center(
-        child: Text("Versão Inicial"),
-      ),
+    return MaterialApp(
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo produto runner',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 200.80,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Versão Inicial"));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Despesas Pessoais"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            child: Card(
+              color: Colors.blue,
+              child: Text("Grafico"),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text("Lista de transações"),
+          )
+        ],
+      ),
+    );
   }
 }
